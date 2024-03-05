499281b941a136f9e3347489554c7188f74a3145
Todor <144096670+teo23062@users.noreply.github.com>
2024-03-05 10:38:31 +0100

    header

--- Sourcecode/header.php
+++ Sourcecode/header.php
@@ -1,1 +1,19 @@
+<header>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alata&family=Concert+One&family=Courgette&display=swap" rel="stylesheet">
+</header>
+
 <h1>Velkommen!</h1>
+
+<style>
+
+    h1 {
+        margin-top: 10%;
+        margin-left: 40%;
+        font-size: 70px;
+        font-family: "Alata", sans-serif;
+        font-style: normal;
+    }
+
+</style>
