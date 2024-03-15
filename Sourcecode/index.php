db689736cabb51a5bd0f1e0b7b73b4ed1c23ce87
Todor <144096670+teo23062@users.noreply.github.com>
2024-03-15 09:25:45 +0100

    index

---
+++ Sourcecode/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bitnami.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        include 'nav.php';
+        include 'header.php';
+        include 'connection.php'
+    ?>
+
+    <div id="button-container">
+        <button id="toggleButton">Merk</button>
+        <a href="rediger.php"><button id="Redigerknapp">Rediger</button></a>
+        <a href="add.php"><button id="LeggtilKnapp">Legg til</button></a>
+    </div>
+
+<?php
+
+echo "<table class='bordered-table'>";
+$counter = 0;
+if ($result = $mysqli->query("SELECT kunde.*, postnummer.poststed AS poststed FROM kunde
+                              LEFT JOIN postnummer ON kunde.postnummer = postnummer.postnummer")) {
+    while ($row = $result->fetch_assoc()) {
+        if ($counter % 5 == 0) {    
+            echo "<tr>"; // Start a new row every five elements
+        }
+        echo "<td style='cursor: pointer;'>";
+        echo "<b>Navn:</b> ", $row["navn"] . "<br>";
+        echo "<b>Postnummer:</b> ", $row["postnummer"]. ", " . $row["poststed"] . "<br>"; // Print poststed instead of postnummer twice
+        echo "<b>Epost:</b> ", $row["epost"] . "<br>";
+        echo "<b>Tlf:</b> ", $row["tlf"] . "<br>";
+        echo "</td>";
+        $counter++;
+        if ($counter % 5 == 0) {
+            echo "</tr>";
+        }
+    }
+}
+echo "</table>";
+?>
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById("toggleButton");
+  var tds = document.querySelectorAll("td");
+
+  button.addEventListener("click", function() {
+    tds.forEach(function(td) {
+      td.addEventListener("click", function() {
+        this.classList.toggle("grey");
+        
+      });
+    });
+  });
+});
+</script>
+
+<?php
+    include 'footer.php';
+?>
+</body>
+</html>
+
+
+    <style>
+        body {
+            background-color: aliceblue;
+        }
+
+        #button-container{
+            margin-left: 74.2%;
+        }
+
+        #LeggtilKnapp{
+            width: 5vw;
+            background-color: #3262ab;
+            color: white;
+        }
+
+        #Redigerknapp {
+            width: 5vw;
+            background-color: #BD0000;
+            color: white;
+            position: relative;
+        }
+
+        #toggleButton{
+            width: 3vw;
+            background-color: grey;
+            color: white;
+
+        }
+
+        button{
+            width: 13vw;
+            cursor: pointer;
+        }
+
+        table {
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 1%;
+            border-collapse: separate;
+            border-spacing: 10px;
+            border: 2px solid black;
+            background-color: white;
+            margin-bottom: 50px;
+        }
+
+        td {
+            border: 2px solid black;
+        }
+
+        .grey {
+            background-color: #E5F3FF;
+        }
+    </style>
